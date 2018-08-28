653ff2ddcc693e59957b8a71528f81f231f85e8d
Cal <chenqibuu@163.com>
2018-08-28 19:44:03 +0800

    Create Project

---
+++ MyStickyNote/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyStickyNote
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StickyNote_UC_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            foreach (UIElement item in NotesGrid.Children)
+            {
+                Grid.SetZIndex(item, 0);
+            } 
+            Grid.SetZIndex(sender as UIElement, 1);
+        }
+    }
+}
